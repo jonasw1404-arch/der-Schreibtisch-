825cad26d13d9ee04b63abafd8ece8609d7e0af6
jonasw1404-arch <jonasw1404@gmail.com>
2025-12-05 23:48:15 +0100

    Update script.js

--- script.js
+++ script.js
@@ -5,8 +5,23 @@ let dataArray;
 const catImg = document.getElementById("catImg");
 const button = document.getElementById("button");
 
+// Musik laden
+const bgMusic = new Audio("music.mp3");
+bgMusic.loop = true;
+bgMusic.volume = 0.4;
+
+// Explosionssound laden
+const explosionSound = new Audio("explosion.wav");
+explosionSound.volume = 1.0;
+
+let explosionPlayed = false; // verhindert Dauerspammen
+
 button.onclick = async () => {
+    // Mikrofonzugriff
     const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
+
+    // Musik starten
+    bgMusic.play();
 
     audioContext = new AudioContext();
     const source = audioContext.createMediaStreamSource(stream);
@@ -17,6 +32,7 @@ button.onclick = async () => {
     source.connect(analyser);
 
     dataArray = new Uint8Array(analyser.frequencyBinCount);
+
     animate();
 };
 
@@ -25,7 +41,7 @@ function animate() {
 
     analyser.getByteFrequencyData(dataArray);
 
-    const volume = dataArray[0];   // Einfacher Lautheitswert (0–255)
+    const volume = dataArray[0]; // einfacher Lautstärkewert 0–255
 
     // 10 Bereiche definieren
     let index = Math.floor((volume / 255) * 10);
@@ -33,8 +49,16 @@ function animate() {
     if (index > 10) index = 10;
 
     // Bild wechseln
-    catImg.style.opacity = 0.6;
-    catImg.src = `imgs/cat${index}.png`;
+    catImg.src = `cat${index}.png`;
 
-    setTimeout(() => catImg.style.opacity = 1, 80);
+    // Explosion-Sound bei cat10
+    if (index === 10) {
+        if (!explosionPlayed) {
+            explosionPlayed = true;
+            explosionSound.currentTime = 0;
+            explosionSound.play();
+        }
+    } else {
+        explosionPlayed = false;
+    }
 }
